96d96fcfc4c7534763dad95d9d302becee4d2d91
liubaorui <qyliubaorui123@163.com>
2020-08-27 20:35:50 +0800

    complete DateUtils v1.0

---
+++ src/main/java/top/webclass/utils/DateUtils.java
@@ -0,0 +1,17 @@
+package top.webclass.utils;
+
+/**
+ * 日期工具类
+ * @author liubaorui
+ * @version 1.0
+ * @date 2020/8/27 20:31
+ */
+public class DateUtils {
+    /**
+     *
+     * @return
+     */
+    public static String getTimestamp() {
+        return null;
+    }
+}
